e75ff0b42d440c2ac906c7cb7961e0b7d4dff848
Diego Franco Portela <dfrancoportela@danielcastelao.org>
2021-01-26 11:22:10 +0100

    A

--- src/com/colaborando/Diego.java
+++ src/com/colaborando/Diego.java
@@ -2,7 +2,7 @@ package com.colaborando;
 
 public class Diego {
     public void amosarDiego () {
-        String mensaje = "Clase de Diego";
+        String mensaje = "Clase de Diego.";
         System.out.println(mensaje);
     }
 }
